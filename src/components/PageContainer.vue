4d016eb712a6fcb9007334ac1cf33d448ac4a35a
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-04-02 14:43:21 +0200

    created page container component

---
+++ src/components/PageContainer.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup />
+
+<template>
+  <div class="page-container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.page-container {
+  align-self: center;
+  max-width: 1200px;
+}
+</style>
